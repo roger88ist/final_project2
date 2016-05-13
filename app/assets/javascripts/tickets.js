723735d326056928e69eee05b46b31f7ca2e9d44
christian abad <cabad20@gmail.com>
2016-05-12 20:11:37 -0400

    changes to the javascript file for google api

--- app/assets/javascripts/tickets.js
+++ app/assets/javascripts/tickets.js
@@ -9,28 +9,7 @@ function initMap() {
     geocodeAddress(geocoder, map);
   });
 }
-window.onload = function() {
-  var startPos;
-  var geoOptions = {
-    enableHighAccuracy: true
-  }
 
-  var geoSuccess = function(position) {
-    startPos = position;
-    document.getElementById('startLat').innerHTML = startPos.coords.latitude;
-    document.getElementById('startLon').innerHTML = startPos.coords.longitude;
-  };
-  var geoError = function(error) {
-    console.log('Error occurred. Error code: ' + error.code);
-    // error.code can be:
-    //   0: unknown error
-    //   1: permission denied
-    //   2: position unavailable (error response from location provider)
-    //   3: timed out
-  };
-
-  navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOptions);
-};
 function geocodeAddress(geocoder, resultsMap) {
   var address = document.getElementById('address').value;
   geocoder.geocode({'address': address}, function(results, status) {
